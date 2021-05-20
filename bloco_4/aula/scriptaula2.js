2ffeefc554d9f6e4b2e228b976ca041a1e49026c
sonkai <mfilho86@gmail.com>
2021-05-20 16:43:05 -0300

    aprendi for e for of

--- bloco_4/aula/scriptaula2.js
+++ bloco_4/aula/scriptaula2.js
@@ -1,5 +1,14 @@
-let lot = [12, 45, 56, 54, 32, 60];
+let lot = [12, 45, 56, 54, 32, 60, 10, 30, 50, 90];
 
 console.log(lot[3]); 
 console.log("tamanho do array, lot é", lot.length);
 
+for (let indexlot = 0; indexlot < lot.length; indexlot+=1) {
+  console.log(lot[indexlot])
+}
+
+console.log("");
+
+for (lotimprime of lot) {
+  console.log(lotimprime);
+}
