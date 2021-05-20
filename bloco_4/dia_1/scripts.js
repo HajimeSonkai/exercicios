3ef36fa9a1ba5fda7b8591a8a3c6c43de67c9861
sonkai <mfilho86@gmail.com>
2021-05-20 15:45:38 -0300

    Programa 6 pecas de xadrez em minusculo e na verdade a mudanca do 5 porque aparentemente eu estava modificando tudo na pasta errada

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -55,23 +55,55 @@ if (ap4 >= 1) {
 
 console.log("");
 
-// NOTE Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus. Um ângulo será considerado inválido se não tiver um valor positivo.
-
+// NOTE  programa 5 Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus. Um ângulo será considerado inválido se não tiver um valor positivo.
+//ainda dava para fazer um else if para cada angulo errado porem eu me respeito!
 let angulo1 = 90;
-let angulo2 = 80;
+let angulo2 = -30;
 let angulo3 = 10;
 let angulosSoma = (angulo1+angulo2+angulo3)
 
-if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0) {
-  switch (angulosSoma) {
-    case 180:
-      console.log("triangulo");
-      break;
-    default:
-      console.log("não triangulo");
-      break;
-  }
-} else {
+if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0 && angulosSoma == 180 ) {
+  console.log("A soma dos angulos, " + angulo1 + ", " + angulo2 + " e " + angulo3 +" é 180, formando um triangulo");
+} else if (angulo1 < 0 | angulo2 < 0 | angulo3 < 0) {
   console.log("angulos invalidos");
+} else {
+  console.log("A soma dos angulos, " + angulo1 + ", " + angulo2 + " e " + angulo3 + " é " + angulosSoma + ", portanto não formando um triangulo");
 }
 
+console.log("");
+
+// NOTE Programa 6 Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz. Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) . Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+
+let str = "num sei";
+let res = str.toLowerCase();
+console.log(res);
+
+switch (res) {
+  case "bispo":
+    console.log("diagonal completa");
+    break;
+
+  case "rainha":
+    console.log("qualquer direção completa");
+    break;
+
+  case "peao":
+    console.log("diagonal 1 casa");
+    break;
+
+  case "cavalo":
+    console.log("movimento em L 3 casas");
+    break;
+
+  case "rei":
+    console.log("Qualquer direção 1 casa");
+    break;
+
+  case "torre":
+    console.log("linha reta completa");
+    break;
+
+  default:
+    console.log("É uma peça invalida, escolha entre, Peao, Torre, Bispo, Cavalo, Rei ou Rainha.");
+    break;
+}
