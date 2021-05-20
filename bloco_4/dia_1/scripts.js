6ab47ed50790995135334abe26bb687c5aa00cf7
sonkai <mfilho86@gmail.com>
2021-05-20 18:57:30 -0300

    progama 7 feito, notas

--- bloco_4/dia_1/scripts.js
+++ bloco_4/dia_1/scripts.js
@@ -107,3 +107,33 @@ switch (res) {
     console.log("É uma peça invalida, escolha entre, Peao, Torre, Bispo, Cavalo, Rei ou Rainha.");
     break;
 }
+
+console.log("");
+
+// NOTE programa 7 Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+//Porcentagem >= 90 -> A
+//Porcentagem >= 80 -> B
+//Porcentagem >= 70 -> C
+//Porcentagem >= 60 -> D
+//Porcentagem >= 50 -> E
+//Porcentagem < 50 -> F
+
+let nota = 96.33;
+
+console.log("sua nota é", nota, "Avaliação:")
+
+if (nota >= 90 && nota < 101) {
+    console.log("Nota A");
+} else if (nota >= 80 && nota < 90) {
+    console.log("Nota B");
+} else if (nota >= 70 && nota < 80) {
+    console.log("Nota C");
+} else if (nota >= 60 && nota < 70) {
+    console.log("Nota D");
+} else if (nota >= 50 && nota < 60) {
+    console.log("Nota E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("Nota F");
+} else {
+    console.log("erro nota deve ser entre 0 e 100");
+}
