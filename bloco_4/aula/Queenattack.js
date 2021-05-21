319b40b7c0b0afd1d050f5781a535605dffcfa42
sonkai <mfilho86@gmail.com>
2021-05-21 17:11:10 -0300

    diagonal esquerda inferior

--- bloco_4/aula/Queenattack.js
+++ bloco_4/aula/Queenattack.js
@@ -1,7 +1,7 @@
 let linhaRainha = 4;
 let colunaRainha = 4;
 
-let linhaOponente = 8;
+let linhaOponente = 0;
 let colunaOponente = 1;
 
 let oportunidade = false;
@@ -19,7 +19,6 @@ for (let isupdir = 1; isupdir < 8; isupdir +=1) {
   let colunaAtualRainha = colunaRainha + isupdir;
 
   if(linhaAtualRainha > 8 || colunaAtualRainha > 8) {
-    console.log("erro de tabuleiro");
     break
   }
   if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
@@ -37,7 +36,6 @@ for (let iSupEsq = 1; iSupEsq < 8; iSupEsq +=1) {
   let colunaAtualRainha = colunaRainha - iSupEsq;
 
   if(linhaAtualRainha > 8 || colunaAtualRainha < 1) {
-    console.log("erro de tabuleiro");
     break
   }
   if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
@@ -49,6 +47,22 @@ console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
 }
 // fim da diagonal superir direita
 
+// NOTE diagonal inferior esquerda
+for (let lInfEsq = 1; lInfEsq < 8; lInfEsq +=1) {
+  let linhaAtualRainha = linhaRainha - lInfEsq;
+  let colunaAtualRainha = colunaRainha - lInfEsq;
+
+  if(linhaAtualRainha < 1 || colunaAtualRainha < 1) {
+    break
+  }
+  if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
+oportunidade = true;
+console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
+  } else {
+    oportunidade = false;
+  }
+}
+// fim da diagonal inferior esquerda
+
 if (oportunidade == false) {
-  console.log("Errou!!!");
 }
