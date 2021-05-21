8771712d430ce5ac23769d27d52690207ae6b5fb
sonkai <mfilho86@gmail.com>
2021-05-21 16:42:46 -0300

    comecando quadrantes no xadres

--- bloco_4/aula/Queenattack.js
+++ bloco_4/aula/Queenattack.js
@@ -1,16 +1,34 @@
 let linhaRainha = 4;
 let colunaRainha = 4;
 
-let linhaOponente = 4;
-let colunaOponente = 7;
+let linhaOponente = 9;
+let colunaOponente = 9;
 
 let oportunidade = false;
 
-if (linhaRainha === linhaOponente || colunaRainha === colunaOponente) {
-  oportunidade = true;
-  console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
-} else {
-  oportunidade = false;
+//if (linhaRainha === linhaOponente || colunaRainha === colunaOponente) {
+//  oportunidade = true;
+//  console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
+//} else {
+//  oportunidade = false;
+//}
+
+for (let isupdir = 1; isupdir < 8; isupdir +=1) {
+  let linhaAtualRainha = linhaRainha + isupdir;
+  let colunaAtualRainha = colunaRainha + isupdir;
+
+  if(linhaAtualRainha > 8 || colunaAtualRainha > 8) {
+    console.log("erro de tabuleiro");
+    break
+  }
+  if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
+oportunidade = true;
+console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
+  } else {
+    oportunidade = false;
+  }
 }
 
-console.log(oportunidade);
+if (oportunidade == false) {
+  console.log("Errou!!!");
+}
