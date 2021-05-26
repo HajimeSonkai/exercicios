859e4f3520d474c78046c4f4f5df5f6cf607bf77
sonkai <mfilho86@gmail.com>
2021-05-26 15:26:07 -0300

    nao sei onde parei

--- bloco_4/aula/Queenattack.js
+++ bloco_4/aula/Queenattack.js
@@ -1,8 +1,8 @@
 let linhaRainha = 4;
 let colunaRainha = 4;
 
-let linhaOponente = 0;
-let colunaOponente = 1;
+let linhaOponente = 1;
+let colunaOponente = 8;
 
 let oportunidade = false;
 
@@ -53,6 +53,23 @@ for (let lInfEsq = 1; lInfEsq < 8; lInfEsq +=1) {
   let colunaAtualRainha = colunaRainha - lInfEsq;
 
   if(linhaAtualRainha < 1 || colunaAtualRainha < 1) {
+    break
+  }
+  if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
+oportunidade = true;
+console.log("Rainha ataca na linha", linhaOponente, ", coluna", colunaOponente);
+  } else {
+    oportunidade = false;
+  }
+}
+// fim da diagonal inferior esquerda
+
+// NOTE diagonal inferior direita
+for (let lInfDir = 1; lInfDir < 8; lInfDir +=1) {
+  let linhaAtualRainha = linhaRainha - lInfDir;
+  let colunaAtualRainha = colunaRainha + lInfDir;
+
+  if(linhaAtualRainha < 1 || colunaAtualRainha > 6) {
     break
   }
   if (linhaOponente === linhaAtualRainha && colunaAtualRainha === colunaAtualRainha) {
