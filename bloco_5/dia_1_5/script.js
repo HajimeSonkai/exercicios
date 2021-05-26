d5c04172328fe7729428604f8b63d17007dd04e3
sonkai <mfilho86@gmail.com>
2021-05-26 19:08:09 -0300

    parte 3 completa

---
+++ bloco_5/dia_1_5/script.js
@@ -0,0 +1,14 @@
+document.getElementById("header-container").style.backgroundColor = "green"
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "pink"
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow"
+
+document.querySelectorAll("h3")[0].style.backgroundColor = "blue"
+document.querySelectorAll("h3")[1].style.backgroundColor = "blue"
+
+document.querySelectorAll("h3")[2].style.backgroundColor = "black"
+document.querySelectorAll("h3")[3].style.backgroundColor = "black"
+
+document.getElementsByTagName("footer")[0].style.backgroundColor = "green"
+document.getElementById("container").style.backgroundColor = "brown"
