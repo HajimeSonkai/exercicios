f05f2d6a0580597d6beacd1a8024abb8825cb46e
sonkai <mfilho86@gmail.com>
2021-05-26 15:48:24 -0300

    inicio do bloco 5

---
+++ bloco_5/script5.1.js
@@ -0,0 +1,9 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "Matrix";
+
+document.getElementById("second-paragraph").innerText = "esse texto é rosa";
+document.getElementById("second-paragraph").style.color = "pink";
+
+document.getElementById("subtitle").style.backgroundColor = "green"
